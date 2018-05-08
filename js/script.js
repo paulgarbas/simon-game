6d756df4518a02904a7c505059488584fa755390
Paulius <garbasauskas@gmail.com>
2018-05-08 22:07:20 +0300

    The same series of quarters signaling with additional step, after correct button is clicked

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 "use strict";
 
 $(document).ready(function() {
-    let space, dashes, number, clicked, randomNumber, quarter;
+    let space, dashes, number, clicked, randomNumber; 
     let turnedOn = false;
     let playerClicked = false;
+    let repeatedClick = false;
+    let clickedWrong = false;
     let nr = 1;
+    let delay = 0;
+    let randomNumberArray = [];
     let audio1 = new Audio('sounds/simonSound1.mp3');
     let audio2 = new Audio('sounds/simonSound2.mp3');
     let audio3 = new Audio('sounds/simonSound3.mp3');
     let audio4 = new Audio('sounds/simonSound4.mp3');
+    let wrongSound = new Audio('sounds/wrong.mp3');
 
     // Turn on/off button
     $(".on-off").click(turnOnOff);
@@ -60,14 +65,15 @@ $(document).ready(function() {
         $(".bottom__left").css("background-color", "rgba(255, 255, 0, 0.7)");
         $(".bottom__right").css("background-color", "rgba(0, 35, 150, 0.7)");
         //Turns off sound
-        audio1.pause();
-        audio2.pause();
-        audio3.pause();
-        audio4.pause();
+        // audio1.pause();
+        // audio2.pause();
+        // audio3.pause();
+        // audio4.pause();
     }
     
     // Starts game
     function start() {
+        randomNumberArray = [];
         dashesDissapear();
         let count = 0;
         // Number resets
@@ -97,13 +103,44 @@ $(document).ready(function() {
 
     function computer() {
         playerClicked = false;
+        clickedWrong = false;
+        let delay = 1;
         number = "0" + nr;
         let twoDigits = number.slice(-2);
         $(".display").text(twoDigits);
-        randomQuarter();
-        console.log(randomNumber);
+        if (repeatedClick) {
+            //Pakartok visus pasirinkimus plius padaryk viena nauja pasirinkima
+            for (let i = 0; i < randomNumberArray.length; i++) {
+                delay++;
+                setTimeout(function() {
+                    lightAndSound(randomNumberArray[i] === 0, randomNumberArray[i] === 1, randomNumberArray[i] === 2, randomNumberArray[i] === 3);
+                    console.log('Pakartorinis', randomNumberArray[i]);
+                }, delay * 1000);
+            }
+        
+            function playRandomQuarter() { 
+                return new Promise(function (resolve, reject) { 
+                    setTimeout(function () { 
+                        resolve(randomQuarter());
+                        console.log('Einantis po pakartotinio, random', randomNumber);              
+                    }, (delay + 1) * 1000);
+                });
+            }
+            
+            function playerTurn() { 
+                return new Promise(function (resolve, reject) { 
+                    resolve(player());
+                });
+            }
+            playRandomQuarter().then(function (resultFromB) {
+                return playerTurn();
+            });
+        } else {
+            randomQuarter();
+            console.log("Random number", randomNumber);
+            player();
+        }
         nr++;
-        player();
     }
 
     function player() {
@@ -114,63 +151,93 @@ $(document).ready(function() {
                 $(this).css("cursor", "pointer");
             });
         }
+        let newRandomNumberArray = randomNumberArray.slice();
+        console.log("newRandomNumberarray ilgis yra: ", newRandomNumberArray.length + ", su siomis reiksmemis: " + newRandomNumberArray);
         // Recognizes which quarter was clicked
-        $(".up__left, .up__right, .bottom__left, .bottom__right").click(function(event){
+        $(".up__left, .up__right, .bottom__left, .bottom__right").off('click').click(function(event){
+            event.stopPropagation();
             if (!playerClicked) {
                 clicked = event.target.className;
-                console.log(event.target.className);
-                lightAndSound(clicked === "up__left", clicked === "up__right", clicked === "bottom__left", clicked === "bottom__right");
-                if (randomNumber === 0 && clicked === "up__left" ||
-                    randomNumber === 1 && clicked === "up__right" ||
-                    randomNumber === 2 && clicked === "bottom__left" ||
-                    randomNumber === 3 && clicked === "bottom__right") {
+                console.log("As paspaudziau", event.target.className);
+                
+                for (let i = 0; i < newRandomNumberArray.length; i++) {
+                    if (newRandomNumberArray[i] === 0 && clicked === "up__left" ||
+                        newRandomNumberArray[i] === 1 && clicked === "up__right" ||
+                        newRandomNumberArray[i] === 2 && clicked === "bottom__left" ||
+                        newRandomNumberArray[i] === 3 && clicked === "bottom__right") {
                         console.log("Paspaudei ta pati!");
+                        lightAndSound(clicked === "up__left", clicked === "up__right", clicked === "bottom__left", clicked === "bottom__right");
+                        newRandomNumberArray.shift();
+                        console.log("newRandomNumberarray ilgis yra: ", newRandomNumberArray.length + ", su siomis reiksmemis: " + newRandomNumberArray);
+                        console.log("randomNumberArray yra ilgis yra: ", randomNumberArray.length + ", su siomis reiksmemis: " + randomNumberArray);
+                        if (newRandomNumberArray.length === 0) {
+                            console.log("Masyvas tapo 0");
+                            playerClicked = true;
+                            repeatedClick = true;
+                            setTimeout(function() {
+                                computer();
+                            }, 2000);
+                        }
+                    } else {
                         playerClicked = true;
+                        repeatedClick = false;
+                        clickedWrong = true;
+                        lightAndSound(clicked === "up__left", clicked === "up__right", clicked === "bottom__left", clicked === "bottom__right");      
+                        wrongSound.play();  
+                        console.log("Ne ta paspaudei!");
                         setTimeout(function() {
-                            computer();
-                        }, 2000)
+                            start();
+                        }, 2000);
+                    }
+                    break;
                 }
-            }
+            } 
         });
     }
     
     // Random quarter with light and sound
     function randomQuarter() {
         randomNumber = Math.floor(Math.random() * 4);
+        randomNumberArray.push(randomNumber);
         lightAndSound(randomNumber === 0, randomNumber === 1, randomNumber === 2, randomNumber === 3);
     }
     
     // Turns light and sound on
-    function lightAndSound(condition1, condition2, condition3, condition4) {
+    function lightAndSound(condition1, condition2, condition3, condition4) {     
         if (condition1) {
             $(".up__left").css("background-color", "rgb(0, 186, 0)");
-            audio1.play();
+            if (!clickedWrong) {
+                audio1.play();
+            }
             setTimeout(function() {
                 $(".up__left").css("background-color", "rgba(0, 128, 0, 0.7)");
             }, 500);
         } else if (condition2) {
             $(".up__right").css("background-color", "rgb(255, 0, 0)");
-            audio2.play();
+            if (!clickedWrong) {
+                audio2.play();
+            }
             setTimeout(function() {
                 $(".up__right").css("background-color", "rgba(255, 0, 0, 0.7)");
-            }, 500);
-            quarter = "rightUp";            
+            }, 500);            
         } else if (condition3) {
             $(".bottom__left").css("background-color", "rgb(255, 255, 0)");
-            audio3.play();
+            if (!clickedWrong) {
+                audio3.play();
+            }
             setTimeout(function() {
                 $(".bottom__left").css("background-color", "rgba(255, 255, 0, 0.7)");
-            }, 500);
-            quarter = "leftBottom";            
+            }, 500);           
         } else if (condition4) {
             $(".bottom__right").css("background-color", "rgb(40, 87, 239)");
-            audio4.play();
+            if (!clickedWrong) {
+                audio4.play();
+            }
             setTimeout(function() {
                 $(".bottom__right").css("background-color", "rgba(0, 35, 150, 0.7)");
-            }, 500);
-            quarter = "rightBottom";            
+            }, 500);            
         }
     }
-
 });
+
 
